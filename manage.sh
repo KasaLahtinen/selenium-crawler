62045aa6042a3178272892d66adb2e51c5507966
Kasa Lahtinen <kasa.lahtinen@gmail.com>
2026-04-25 10:48:11 +0300

    refactor: update BASE_DIR to use relative path based on script location

--- manage.sh
+++ manage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-BASE_DIR="/home/kasa/projects/selenium-crawler"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$BASE_DIR"
 
 echo "=========================================="
